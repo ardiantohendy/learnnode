6445059a7ba0ce7dfe982f968048ff27e992d0cf
Ardiantohendy <bayuardianto421@gmail.com>
2023-09-09 10:54:42 +0700

    new commit

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 const port = 3000;
 
+app.use(bodyParser.json());
+
 app.get("/mainpage", (req, res) => {
+  console.log({ urlParam: req.query });
   res.send("Hello World");
+});
+
+app.post("/login", (req, res) => {
+  console.log({ requestFromOutside: req.body });
+  res.send("Login Berhasil...");
+});
+
+app.put("/updateuser", (req, res) => {
+  console.log({ updateData: req.body });
+  res.send("update berhasil...");
 });
 
 app.listen(port, () => {
